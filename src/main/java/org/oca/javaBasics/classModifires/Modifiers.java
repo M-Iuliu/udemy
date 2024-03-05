cd9e19c75c99fe9dd151cdbbc70c94251fc82fed
M-Iuliu <miuliuemanuel@gmail.com>
2024-03-05 18:49:32 +0200

    Chapter 1 udemy-learning

---
+++ src/main/java/org/oca/javaBasics/classModifires/Modifiers.java
@@ -0,0 +1,26 @@
+package org.oca.javaBasics.classModifires;
+
+// top level classes can be public and default
+public class Modifiers {
+
+//    inner classes can have any access modifiers
+    public class innerClass1{
+
+    }
+
+    class innerClass2{
+
+    }
+
+    protected class innerClass3{
+
+    }
+
+    private class innerClass4{
+
+    }
+
+
+}
+
+
