8112043478de7f29e3e8fb2aa13b2c6823139dd8
andreeaRg <radulescu.andreea98@gmail.com>
2024-03-05 18:47:03 +0200

    enum

---
+++ src/main/java/org/oca/javaBasics/classStructure/Enum/TestEnum.java
@@ -0,0 +1,37 @@
+package org.oca.javaBasics.classStructure.Enum;
+
+enum Animal {
+    DOG("whoof"), CAT ("MIAW"), FISH("BOUBLE");
+
+    String sound;
+
+     Animal(String sound){
+         this.sound = sound;
+     }
+
+}
+
+public class TestEnum {
+    static Animal a;
+    public enum Days { Mon, Thu, Wed};
+    public static void main(String[] args) {
+        System.out.println( a.DOG.sound + " " + a.FISH.sound);
+
+        Days[] d = Days.values();
+        System.out.println(d[2]);
+    }
+}
+
+
+abstract class  A {
+
+}
+
+final class  B {
+
+}
+
+
+class  C {
+
+}
